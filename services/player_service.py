2cf981f40004a81a139b4bc7bc5cdc552292ccff
Michael Swisa <ms0504109182@gmail.com>
2024-09-19 11:35:33 +0300

    Added functions that bring all the players of the selected years and load them into a json file after group

--- services/player_service.py
+++ services/player_service.py
@@ -1,14 +1,32 @@
 import my_request
+from itertools import groupby
 import files
 
 
-def load_data_players(*args):
+def load_data_players_from_request(*args):
     players = []
     for year in args:
         response = my_request.get_players(year)
         players.extend(response)
-    files.write_json(f'players.json', players)
+    return players
 
+def sorted_json_players(players):
+    sorted_players = sorted(players, key=lambda player: player['playerName'])
+    return sorted_players
+def group_players_by_name(players):
+    grouped_players = {}
+
+def group_players_by_name(players):
+    grouped_players = {}
+    for key, value in groupby(players, key=lambda player: player['playerName']):
+        grouped_players[key] = list(value)
+    return grouped_players
+
+def save_to_json(data, filename):
+    files.write_json(filename, data)
 
 if __name__ == '__main__':
-    load_data_players(2022, 2023, 2024)
+    players = load_data_players_from_request(2022, 2023, 2024)
+    players_sorted = sorted_json_players(players)
+    players_grouped = group_players_by_name(players_sorted)
+    save_to_json(players_grouped, 'players_grouped.json')
